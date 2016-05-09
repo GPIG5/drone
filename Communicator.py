0550e251ab510f38ae3a7273b1463fb88b138f01
tapuu <mjm540@york.ac.uk>
2016-05-09 16:16:56 +0100

    communicator has now been tested and debugged, so it probably works

--- Communicator.py
+++ Communicator.py
@@ -10,13 +10,13 @@ class Communicator:
 		self.writer = writer
 	@asyncio.coroutine
 	def send(self, data):
-		encoded = json.dumps(data).encode()
-		yield from self.writer.write(struct.pack("!L", len(encoded)))
-		yield from self.writer.write(encoded)
+		encoded = json.dumps(data).encode('utf-8')
+		self.writer.write(struct.pack("!L", len(encoded)))
+		self.writer.write(encoded)
 	@asyncio.coroutine
 	def receive(self):
 		unencoded_size = yield from self.reader.readexactly(4)
-		encoded_size = struct.unpack("!L", unencoded_size)
+		encoded_size = struct.unpack("!L", unencoded_size)[0]
 		unencoded_data = yield from self.reader.readexactly(encoded_size)
 		encoded_data = json.loads(unencoded_data.decode())
 		return encoded_data
