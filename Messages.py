72dcad03e80fe0ecc6de33c887a158991cc835d7
tapuu <mjm540@york.ac.uk>
2016-05-09 14:13:01 +0100

    more messages (untested)

--- Messages.py
+++ Messages.py
@@ -7,21 +7,34 @@ class Point:
 		return {"x": self.x, "y": self.y}
 
 class Message:
-	def __init__(self, uuid, t, data):
+	def __init__(self, uuid, t):
 		self.uuid = uuid
 		self.type = t
-		self.data = data
 	def to_json(self):
-		return {"uuid":self.uuid,"type":self.type,"data":self.data.to_json()}
+		return {"uuid":self.uuid,"type":self.type,"data":{}}
 
 class MeshMessage(Message):
-	def __init__(self, uuid, data):
-		Message.__init(uuid, "mesh", data)
+	def __init__(self, uuid, origin):
+		self.origin = origin
+		Message.__init(uuid, "mesh")
 	def to_json(self):
-		return Message.to_json()
+		d = Message.to_json(self)
+		d["data"]["origin"] = self.origin
+		return d
 
 class DirectMessage(Message):
-	def __init__(self, uuid, data):
-		Message.__init(uuid, "direct", data)
+	def __init__(self, uuid):
+		Message.__init(uuid, "direct")
 	def to_json(self):
-		return Message.to_json()
+		return Message.to_json(self)
+
+class StatusMesh(MeshMessage):
+	def __init__(self, location, battery):
+		self.location = location
+		self.battery = battery
+	def to_json(self):
+		d = MeshMessage.to_json(self)
+		d["data"]["location"] = self.location.to_json()
+		d["data"]["battery"] = self.battery
+		d["data"]["datatype"] = "status"
+		return d
