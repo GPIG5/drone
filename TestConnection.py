181c161919f2a90d096e522cb5d3d12d948cb9df
tapuu <mjm540@york.ac.uk>
2016-05-09 15:31:21 +0100

    added print statements for debugging and switched to string instead of dictionary as test data

--- TestConnection.py
+++ TestConnection.py
@@ -1,13 +1,16 @@
-from Communicator import *
+import Communicator
+import asyncio
 
 @asyncio.coroutine
 def main(host):
-	c = Communicator()
+	c = Communicator.Communicator()
 	yield from c.connect(host)
-	yield from c.send({"test": "hello"})
+	print("connected")
+	yield from c.send("hello")
+	print("sent")
 	m = yield from c.receive()
 	print(m)
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main("lol"))
+loop.run_until_complete(main("10.240.149.164"))
 loop.close()
