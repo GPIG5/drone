508d5db4a7ff8568adcc78b44612b891b62e9697
AJDurant <andy@ajdurant.co.uk>
2016-05-11 15:53:06 +0100

    3.4 syntax

--- communicator.py
+++ communicator.py
@@ -11,24 +11,28 @@ class Communicator:
         self.host = self.config.get('host')
         self.port = self.config.getInt('port')
 
-    async def initialise():
-        await connect(self.host, self.port)
+    @asyncio.coroutine
+    def initialise():
+        yield from connect(self.host, self.port)
         hello = DirectMessage(self.drone.getUUID())
-        await send(hello)
+        yield from send(hello)
 
-	async def connect(self, host, port):
-		reader, writer = await asyncio.open_connection(host, port)
+	@asyncio.coroutine
+    def connect(self, host, port):
+		reader, writer = yield from asyncio.open_connection(host, port)
 		self.reader = reader
 		self.writer = writer
 
-	async def send(self, data):
+	@asyncio.coroutine
+    def send(self, data):
 		encoded = json.dumps(data).encode('utf-8')
 		self.writer.write(struct.pack("!L", len(encoded)))
 		self.writer.write(encoded)
 
-	async def receive(self):
-		unencoded_size = await self.reader.readexactly(4)
+	@asyncio.coroutine
+    def receive(self):
+		unencoded_size = yield from self.reader.readexactly(4)
 		encoded_size = struct.unpack("!L", unencoded_size)[0]
-		unencoded_data = await self.reader.readexactly(encoded_size)
+		unencoded_data = yield from self.reader.readexactly(encoded_size)
 		encoded_data = json.loads(unencoded_data.decode())
 		return encoded_data
