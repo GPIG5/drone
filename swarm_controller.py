68d638276c2d83cfb7102c92b9e07cbe218807bc
disblader <domas123123@gmail.com>
2016-05-15 13:47:54 +0100

    Swarm (almost) complete

--- swarm_controller.py
+++ swarm_controller.py
@@ -21,33 +21,37 @@ class SwarmController(Layer):
 
         self.avoidance_radius = config.getint('avoidance_radius')
         self.aggregation_timeout = config.getint('aggregation_timeout')
-        self.avoid_until_range = config.getint('avoid_until_range')
+        self.target_radius = config.getint('target_radius')
 
     def execute_layer(self, current_output):
         if self.state == State.normal:
             # 1. Check if avoidance needed
             if self.avoidance_needed():
                 return self.perform_avoidance()
-
             # 2. Check if coherence needed
-            if self.coherence_needed():
+            elif self.coherence_needed():
                 return self.perform_coherence()
-
             # 3. Continue to searching layer
-            return self.next.execute_layer(current_output)
-        elif self.state == State.coherence:
-            # If coherence complete return to normal; otherwise continue with coherence
-            if self.coherence_complete():
-                self.perform_normal()
             else:
-                return self.perform_coherence
+                return self.perform_normal(current_output)
+
+        elif self.state == State.coherence:
+            if self.avoidance_needed():
+                return self.perform_avoidance()
+            elif self.coherence_complete():
+                # If coherence complete return to normal; otherwise continue with coherence
+                self.aggregation_timer = time.time()
+                self.perform_normal(current_output)
+            else:
+                return self.perform_coherence()
 
         elif self.state == State.avoidance:
             # If avoidance complete return to normal; otherwise continue with avoidance
             if self.avoidance_complete():
-                self.perform_normal()
+                self.aggregation_timer = time.time()
+                self.perform_normal(current_output)
             else:
-                return self.perform_coherence
+                return self.perform_coherence()
 
     # Checks whether an avoidance move is necessary in the current state
     def avoidance_needed(self):
@@ -72,127 +76,39 @@ class SwarmController(Layer):
         self.aggregation_timer = time.time()
         return Action(self.target)
 
+    def avoidance_complete(self):
+        return self.telemetry.get_location().distance_to(self.target) < self.telemetry
+
     def coherence_needed(self):
-        pass
+        return self.aggregation_timer - time.time() > self.aggregation_timeout
 
     def perform_coherence(self):
-        pass
+
+        if self.state != State.coherence:
+            # If avoidance was just initiated, we need to calculate which way to avoid to
+            self.state = State.coherence
+
+            neighbours_in_range = [None, None, None]  # TODO - positions of neighbours in range
+
+            # We find the center of mass by averaging. Mass of all points is considered 1
+            totalmass = 0
+            total_x = 0
+            total_y = 0
+            total_z = 0
+            for i in range(len(neighbours_in_range)):
+                totalmass += 1
+                total_x += neighbours_in_range[i].x
+                total_y += neighbours_in_range[i].y
+                total_z += neighbours_in_range[i].z
+
+            self.target = Point(total_x / totalmass, total_y / totalmass, total_z / totalmass)
+
+        return Action(self.target)
 
     def coherence_complete(self):
-        pass
+        return self.telemetry.get_location().distance_to(self.target) < self.telemetry
 
-    def perform_normal(self):
-        pass
-
-    def avoidance_complete(self):
-        pass
-
-# class Navigator2:
-#     def __init__(self, self_id, grid_state, telemetry, neighbours, engine):
-#         self.id = self_id
-#         self.state = State.move
-#         self.grid = grid_state
-#         self.telemetry = telemetry
-#         self.neighbours = neighbours
-#         self.engine = engine
-#
-#     def loop(self):
-#         # TODO enough battery life?
-#         if self.state == State.move:
-#             self.when_move()
-#         elif self.state == State.search:
-#             self.when_search()
-#         elif self.state == State.avoid:
-#             self.when_avoid()
-#         elif self.state == State.recharge:
-#             self.when_recharge()
-#         elif self.state == State.coherence:
-#             self.when_coherence()
-#         elif self.state == State.rendezvous:
-#             self.when_rendezvous()
-#
-#     def when_move(self):
-#         pass
-#
-#     def when_search(self):
-#         pass
-#
-#     def when_recharge(self):
-#         pass
-#
-#     def when_cohere(self):
-#         pass
-#
-#     def when_avoid(self):
-#         pass
-#
-#     def when_coherence(self):
-#         pass
-#
-#     def when_rendezvous(self):
-#         pass
-#
-#
-# # Omega-algorithm based navigator
-# # "Avoidance" refers to 2 circumstances (and causes different reactions):
-# #   - When you get too close to another drone:  turn away from the direction of this droid and fly for a bit then
-# #       recalculate
-# #   - When you arrive to a sector and it is occuppied: recalculate target
-# #
-# #
-# class OmegaNavigator(Navigator):
-#     def __init__(self, self_id, grid_state, telemetry, neighbours, engine):
-#         super().__init__(self_id, grid_state, telemetry, neighbours, engine)
-#         self.avoidance_target = None
-#         self.aggregation_timer = time.time()
-#         # TODO initialise target
-#         # target = the sector as identified by the grid coordinates
-#         self.target = None
-#
-#         config = configparser.ConfigParser()
-#         config.read('navigator.cfg')
-#         omega = config['Omega']
-#
-#         self.avoidance_radius = omega['avoidance_radius']
-#         self.aggregation_timeout = omega['aggregation_timeout']
-#         self.avoid_until_range = omega['avoid_until_range']
-#
-#     def when_move(self):
-#         # 1. Check for avoidance
-#         # TODO doesn't account for multiple neighbours in range
-#         current_position = self.telemetry.current_position()
-#         closest_neighbour = self.neighbours.closest_to(current_position)
-#
-#         if current_position.distance_to(self.neighbours[closest_neighbour].position) < self.avoidance_radius:
-#             self.avoidance_target = closest_neighbour
-#             self.state = State.avoid
-#             pass
-#
-#         # 2. Check if coherence move is necessary
-#         if self.aggregation_timer - time.time() > self.aggregation_timeout:
-#             self.state = State.coherence
-#             pass
-#
-#         # 3. Check if arrived; switch to search or recalculate
-#         x, y = self.target[1], self.target[2]
-#         if self.grid.position_within_sector(x, y, current_position):
-#             # We have arrived!
-#             if self.grid.grid[x][y] == datastore.SquareState.notSearched:
-#                 #TODO communicate the search update
-#                 #TODO datastore update - should be thread safe
-#                 self.grid.grid[x][y] = self.id
-#                 self.state = State.search
-#                 pass
-#             else:
-#                 self.recalculate_target()
-#
-#         # 4. If we're still trying to move, move towards target
-#         self.engine.moveTowards(datastore.Position(self.target[1], self.target[2], current_position.z))
-#
-#     # Performs the avoidance maneuver away from the specified neighbour
-#     def when_avoid(self):
-#         pass
-#
-#     def recalculate_target(self):
-#         pass
-#
+    def perform_normal(self, current_output):
+        if self.state != State.normal:
+            self.state = State.normal
+        return self.next.execute_layer(current_output)
