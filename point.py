c7c4d1fe4e9aa864345a3699792024ad9403277a
tapuu <mjm540@york.ac.uk>
2016-05-16 14:24:12 +0100

    changing to right json format

--- point.py
+++ point.py
@@ -8,9 +8,9 @@ class Point:
     def from_json(cls, d, self=None):
         if self == None:
             self = cls.__new__(cls)
-        self.p.longitude = d["longitude"]
-        self.p.latitude = d["latitude"]
-        self.p.altitude = d["altitude"]
+        self.p.longitude = d["long"]
+        self.p.latitude = d["lat"]
+        self.p.altitude = d["alt"]
         return self
     @property
     def latitude(self):
@@ -23,9 +23,9 @@ class Point:
         return self.p.latitude
     def to_json(self):
         return {
-            "longitude": self.p.longitude,
-            "latitude": self.p.latitude,
-            "altitude": self.p.altitude
+            "long": self.p.longitude,
+            "lat": self.p.latitude,
+            "alt": self.p.altitude
         }
     def distance_to(self, p2):
         return geopy.distance.great_circle(self.p, p2.p).meters
