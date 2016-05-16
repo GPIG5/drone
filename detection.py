300cd41f6169bdbc8d83a65e6b0f51c08d546bee
Andy Durant <andy@ajdurant.co.uk>
2016-05-16 16:18:42 +0100

    detection complete

--- detection.py
+++ detection.py
@@ -12,7 +12,6 @@ class Detection:
         self.pinor_file = self.data_folder + 'pinor.csv'
         self.communicator = communicator
         self.messagedispatcher = messagedispatcher
-        self.pinor = []
 
     @asyncio.coroutine
     def initialise(self):
@@ -28,29 +27,27 @@ class Detection:
             msg = yield from self.messagedispatcher.wait_for_message('direct', 'pinor')
             msg = msg.to_json()
 
-            timestamp = time.time()
             timestr = time.strftime('%Y%m%d%H%M%S')
-            pinor = (timestamp, msg['data']['pinor'])
+            pinors = msg['data']['pinor']
             img64 = msg['data']['img']
 
             # Write image to file
-            f = yield from aiofiles.open(self.data_folder + 'images/' + timestr + '.jpg', mode='w')
+            f = yield from aiofiles.open(self.data_folder + 'images/' + timestr + '.jpg', mode='wb')
             try:
-                yield from f.write(base64.decodestring(img))
+                yield from f.write(base64.decodestring(img64.encode()))
             finally:
                 yield from f.close()
 
             # Write co-ords to file
             f = yield from aiofiles.open(self.pinor_file, mode='w')
             try:
-                for pinor in msg['data']['pinor']:
+                for pinor in pinors:
                     point = pinor.to_json()
                     yield from f.write(timestamp + ',' + point['lat'] + ',' + point['lon'] + ',' + point['alt'] + ',' + timestr + '.jpg' + '\n')
             finally:
                 yield from f.close()
 
-            self.pinor.append(pinor)
-            yield from self.communicator.send(PinorMesh(self.uuid, self.uuid, pinor).to_json())
+            yield from self.communicator.send(PinorMesh(self.uuid, self.uuid, pinors).to_json())
 
     def get_pinor():
         return self.pinor
