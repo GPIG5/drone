98e375e445d0bbf8965556910bda14cdc1e014a4
Domas Poliakas <domas123123@gmail.com>
2016-05-18 14:50:59 +0100

    Switched from class to tuple

--- datastore.py
+++ datastore.py
@@ -1,7 +1,7 @@
-from enum import Enum
 import asyncio
-
 import itertools
+from enum import Enum
+from point import Point
 
 
 class Drone:
@@ -40,25 +40,23 @@ class SectorState(Enum):
     shouldNotSearch = 3
     being_searched = 4
 
+# class SectorIndex:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+#     def __eq__(self, other):
+#         if isinstance(other, self.__class__):
+#             return self.x == other.x and self.y == other.y
+#         return False
+#
+#     def __ne__(self, other):
+#         return not self.__eq__(other)
+#
+#     def __hash__(self):
+#         return self.x * self.y
 
-class SectorIndex:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
 
-    def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            return self.x == other.x and self.y == other.y
-        return False
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __hash__(self):
-        return self.x * self.y
-
-
-# gridState.grid[x][y] - status of the (x,y) sector
 class GridState:
     def __init__(self, bottom_left, top_right, detection_radius):
         self.origin = bottom_left
@@ -75,42 +73,56 @@ class GridState:
         self.sector_height = map_height / self.y_count
         self.sector_width = map_width / self.x_count
 
-        self.sector_state = {SectorIndex(i, j): SectorState.notSearched
+        self.sector_state = {(i, j): (SectorState.notSearched, 0)
                              for i, j in itertools.product(range(self.x_count), range(self.x_count))}
 
-        # Checks whether the given position is within the sector
-        # def position_within_sector(self, sector_index, position):
-        #     leftmost_latitude = sector_index.x
-        #     rightmost_x = x_of_sector + self.square_width
-        #     bottom_y = y_of_sector
-        #     top_y = y_of_sector + self.square_height
-        #     return position.x > leftmost_x & position.x < rightmost_x & position.y < top_y & position.y > bottom_y
+    # Checks whether the given position is within the sector
+    def position_within_sector(self, sector_index, position):
+        bottom_left = self.get_sector_origin(sector_index)
+        top_right = Point(bottom_left.longitude + self.sector_height,
+                          bottom_left.latitude + self.sector_width,
+                          self.origin.altitude)
 
+        return position.latitude > bottom_left.latitude & \
+                                   position.latitude < top_right.latitude & \
+                                                       position.longitude > bottom_left.longitude & \
+                                                                            position.longitude < top_right.longitude
 
-class Neighbours:
-    def __init__(self, number_of_neighbours):
-        self.neighbours = [None for i in range(number_of_neighbours)]
+    def state_for(self, sector_index):
+        return self.sector_state[sector_index][0]
 
-    def update_neighbour(self, index, position):
-        if self.neighbours[index] is not None:
-            self.neighbours[index].update_position(position)
-        else:
-            self.neighbours[index] = NeighbourState(position)
+    def drone_of(self, sector_index):
+        return self.sector_state[sector_index][1]
 
-    # Finds the closest neighbour to the defined position
-    def closest_to(self, position):
-        # Find distances; fill -1 if no neighbour state defined for the given neighbour
-        distances = [position.distance_to(self.neighbours[i].position) if self.neighbours[i] is not None else -1
-                     for i in range(len(self.neighbours))]
-        # Eliminate the -1s arising from undefined neighbour states
-        valid_distances = [distances[i] if distances[i] != -1 else max(distances) for i in range(len(self.neighbours))]
-        return distances.index(min(valid_distances))
+    def get_sector_origin(self, sector_index):
+        latitude = self.origin.latitude + sector_index.x * self.sector_width
+        longitude = self.origin.longitude + sector_index.y * self.sector_height
+        return Point(longitude, latitude, self.origin.altitude)
 
-
-# Neighbour positions
-class NeighbourState:
-    def __init__(self, position):
-        self.position = position
-
-    def update_position(self, position):
-        self.position = position
+# class Neighbours:
+#     def __init__(self, number_of_neighbours):
+#         self.neighbours = [None for i in range(number_of_neighbours)]
+#
+#     def update_neighbour(self, index, position):
+#         if self.neighbours[index] is not None:
+#             self.neighbours[index].update_position(position)
+#         else:
+#             self.neighbours[index] = NeighbourState(position)
+#
+#     # Finds the closest neighbour to the defined position
+#     def closest_to(self, position):
+#         # Find distances; fill -1 if no neighbour state defined for the given neighbour
+#         distances = [position.distance_to(self.neighbours[i].position) if self.neighbours[i] is not None else -1
+#                      for i in range(len(self.neighbours))]
+#         # Eliminate the -1s arising from undefined neighbour states
+#         valid_distances = [distances[i] if distances[i] != -1 else max(distances) for i in range(len(self.neighbours))]
+#         return distances.index(min(valid_distances))
+#
+#
+# # Neighbour positions
+# class NeighbourState:
+#     def __init__(self, position):
+#         self.position = position
+#
+#     def update_position(self, position):
+#         self.position = position
