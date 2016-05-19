588b77bfd1678c05f11b810883827d080d347589
Domas Poliakas <domas123123@gmail.com>
2016-05-19 16:43:43 +0100

    Testing

--- collision_detector.py
+++ collision_detector.py
@@ -9,13 +9,13 @@ class CollisionDetector(Layer):
 
     def execute_layer(self, current_output):
         op = Layer.execute_layer(self, current_output)
-        collide = None
-        collide_dist = None
-        for uuid, drone in self.data_store.all_drones().items():
-            d = drone.location.distance_to(self.telemetry.get_location())
-            if collide is None or d < collide_dist:
-                collide = drone.location
-                collide_dist = d
-        if collide is not None and collide_dist <= 10:
-            op.move = self.telemetry.get_location().perp(collide)
+        # collide = None
+        # collide_dist = None
+        # for uuid, drone in self.data_store.all_drones().items():
+        #     d = drone.location.distance_to(self.telemetry.get_location())
+        #     if collide is None or d < collide_dist:
+        #         collide = drone.location
+        #         collide_dist = d
+        # if collide is not None and collide_dist <= 10:
+        #     op.move = self.telemetry.get_location().perp(collide)
         return op
