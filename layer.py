26295147443247f1856576f26dc7549711cb2d18
Domas Poliakas <domas123123@gmail.com>
2016-05-19 12:54:01 +0100

    config + bugs

--- layer.py
+++ layer.py
@@ -1,20 +1,26 @@
 from point import Point
 
+
 class Layer:
     def __init__(self, next):
         self.next = next
+
     def execute_layer(self, current_output):
         current_output = self.next(current_output)
         return current_output
 
+
 class Action:
-    def __init__(self, move = None, take_picture = None, claim_sector = None):
+    def __init__(self, move=None, take_picture=None, claim_sector=None):
         self.move = move
         self.take_picture = take_picture
         self.claim_sector = claim_sector
+
     def has_move(self):
-        return self.move != None
+        return self.move is not None
+
     def has_take_picture(self):
-        return self.take_picture != None
+        return self.take_picture is not None
+
     def has_claim_sector(self):
-        return self.claim_sector != None
+        return self.claim_sector is not None
