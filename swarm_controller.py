656ef6f571421fdfc7a8574b40338c6bc637b7d6
David Wotherspoon <david@dwotherspoon.com>
2016-06-02 00:26:29 +0100

    Remove swarm [sorry Domas :(]

--- swarm_controller.py
+++ swarm_controller.py
@@ -10,8 +10,7 @@ from geopy.distance import great_circle
 
 class State(Enum):
     normal = 1
-    coherence = 2
-    avoidance = 3
+    avoidance = 2
 
 
 class SwarmController(Layer):
@@ -29,7 +28,6 @@ class SwarmController(Layer):
         self.target_radius = config.getint('target_radius')
         self.radio_radius = config.getint('radio_radius')
         self.drone_timeout = config.getint('drone_timeout')
-        self.cohesion_degree = config.getint('cohesion_degree')/100
         self.critical_avoidance_range = config.getint('critical_avoidance_range')
 
     def execute_layer(self, current_output):
@@ -40,23 +38,8 @@ class SwarmController(Layer):
             # 1. Check if avoidance needed
             if self.avoidance_needed():
                 return self.perform_avoidance(current_output)
-            # 2. Check if coherence needed
-            elif self.coherence_needed():
-                return self.perform_coherence(current_output)
-            # 3. Continue to searching layer
             else:
                 return self.perform_normal(current_output)
-
-        elif self.state == State.coherence:
-            if self.avoidance_needed():
-                return self.perform_avoidance(current_output)
-            elif self.coherence_complete():
-                # If coherence complete return to normal; otherwise continue with coherence
-                self.aggregation_timer = time.time()
-                return self.perform_normal(current_output)
-            else:
-                return self.perform_coherence(current_output)
-
         elif self.state == State.avoidance:
             # If avoidance complete return to normal; otherwise continue with avoidance
             if self.avoidance_complete():
@@ -122,98 +105,6 @@ class SwarmController(Layer):
         return current_output
 
     def avoidance_complete(self):
-        return self.telemetry.get_location().distance_to(self.target) < self.target_radius
-
-    def coherence_needed(self):
-        # The requirements for coherence are an extension of the very rudimentary requirements specified in the omega
-        # algorithm to accomodate the more complex function of the swarm.
-        # The idea is that if the aggregation timer runs out, we check whether the center
-        # of mass would be within radio range if another aggregation timer ran out
-
-        # return time.time() - self.aggregation_timer > self.aggregation_timeout
-        if time.time() - self.aggregation_timer > self.aggregation_timeout:
-            current_position = self.telemetry.get_location()
-            position_of_closest = self.data_store.get_position_of_drone_closest_to(current_position,
-                                                                                   timeout=self.drone_timeout)
-            # center_of_mass = self.compute_neighbour_mass_center()
-            if position_of_closest is not None:
-                distance_to_closest = position_of_closest.distance_to(current_position)
-                if distance_to_closest < self.radio_radius*0.5:
-                    self.aggregation_timer = time.time()
-                else:
-                    return True
-            else:
-                return True
-        else:
-            return False
-
-    def perform_coherence(self, current_output):
-        if self.state != State.coherence:
-            print("Coherence initiated")
-            self.state = State.coherence
-
-        if self.coherence_needed():
-            current_position = self.telemetry.get_location()
-            center_of_mass = self.compute_neighbour_mass_center()
-
-            if center_of_mass is not None:
-                # We move only partially towards the center of mass
-                self.target = Point(
-                    latitude=current_position.latitude +
-                             (center_of_mass.latitude - current_position.latitude) * self.cohesion_degree,
-                    longitude=current_position.longitude +
-                              (center_of_mass.longitude - current_position.longitude) * self.cohesion_degree,
-                    altitude=current_position.altitude +
-                             (center_of_mass.altitude - current_position.altitude) * self.cohesion_degree)
-            else:
-                # If no neighbours in range, move towards the initial position
-                print("FRIENDS LOST")
-                initial_position = self.telemetry.get_initial_location()
-                bearing_to_initial = current_position.bearing_to_point(initial_position)
-
-                towards_home = current_position.point_at_vector(self.radio_radius/2, bearing_to_initial)
-
-                if towards_home.distance_to(initial_position) > current_position.distance_to(initial_position):
-                    self.target = initial_position
-                else:
-                    self.target = towards_home
-
-            # print("COHERENCE INITIATED TOWARDS: " + str(self.target) +
-                  # "CURRENT POSITION: " + str(current_position) +
-                  # "DISTANCE: " + str(self.target.distance_to(current_position)))
-
-        current_output.move = self.target
-        if hasattr(current_output.move, 'simple_string'):
-            current_output.move_info = "COHERENCE MOVE: " + current_output.move.simple_string()
-        else:
-            current_output.move_info = "COHERENCE MOVE"
-        return current_output
-
-    def compute_neighbour_mass_center(self):
-        current_position = self.telemetry.get_location()
-        neighbours_in_range = self.data_store.drones_in_range_of(current_position, self.radio_radius,
-                                                                 timeout=self.drone_timeout)
-
-        if len(neighbours_in_range) != 0:
-            # We find the center of mass by averaging. Mass of all points is considered 1
-            totalmass = 0
-            total_latitude = 0
-            total_longitude = 0
-            total_altitude = 0
-            for i in range(len(neighbours_in_range)):
-                totalmass += 1
-                total_latitude += neighbours_in_range[i].latitude
-                total_longitude += neighbours_in_range[i].longitude
-                total_altitude += neighbours_in_range[i].altitude
-
-            return Point(
-                latitude=total_latitude / totalmass,
-                longitude=total_longitude / totalmass,
-                altitude=total_altitude / totalmass)
-        else:
-            return None
-
-    def coherence_complete(self):
         return self.telemetry.get_location().distance_to(self.target) < self.target_radius
 
     def perform_normal(self, current_output):
